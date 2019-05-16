3ac230c1f564094e19670bd03e434093ff39e6e5
b0ndurant <martin.aurelien01@gmail.com>
2019-05-16 14:45:36 +0200

    exo7 React, create form for update api

---
+++ src/FormFilm.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import "./Form.css"
+
+class FormFilm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            poster: '',
+            comment: '',
+        }
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    submitForm = (e) => {
+        e.preventDefault();
+        const config = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.state),
+        };
+
+        const url = "http://campus-bordeaux.ovh:3001/api/quests/movies/";
+
+        fetch(url, config)
+            .then(res => res.json())
+            .then(res => {
+                if (res.error) {
+                    alert(res.error);
+                } else {
+                    alert(`Added your favorite film with the ID ${res}!`);
+                }
+            }).catch(e => {
+                console.error(e);
+                alert('Error during update film');
+            });
+    }
+
+
+    render() {
+        return (
+            <>
+                <h1 className="text-center mt-3">Formulaire</h1>
+                <form onSubmit={this.submitForm} className="mt-5">
+                    <div className="form-group">
+                        <label>Your name film :</label>
+                        <input type="text" className="form-control" name="name" value={this.state.name} onChange={this.onChange} placeholder="Enter your film" />
+                    </div>
+                    <div className="form-group">
+                        <label>Your Poster film :</label>
+                        <input type="url" className="form-control" name="poster" value={this.state.poster} onChange={this.onChange} placeholder="your poster film" />
+                    </div>
+                    <div className="form-group">
+                        <label>Your comment :</label>
+                        <textarea className="form-control" name="comment" value={this.state.comment} onChange={this.onChange} placeholder="your comment ..." />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </>
+        );
+    }
+}
+
+export default FormFilm;
