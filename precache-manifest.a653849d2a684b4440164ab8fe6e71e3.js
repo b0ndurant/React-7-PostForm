2068a0a0ba0e50c36994f85b9b87c63ffa8fd162
b0ndurant <martin.aurelien01@gmail.com>
2019-05-16 14:48:25 +0200

    Updates

---
+++ precache-manifest.a653849d2a684b4440164ab8fe6e71e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faec9ee8cbb1073f0408bf64f5487672",
+    "url": "/React-7-PostForm/index.html"
+  },
+  {
+    "revision": "c86684450d2e1cea1b42",
+    "url": "/React-7-PostForm/static/css/main.3ea06753.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/React-7-PostForm/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "c86684450d2e1cea1b42",
+    "url": "/React-7-PostForm/static/js/main.20752e8a.chunk.js"
+  },
+  {
+    "revision": "06887a05592fead34844",
+    "url": "/React-7-PostForm/static/js/runtime~main.9683aa02.js"
+  }
+]);
